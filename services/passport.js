5b7fd6668be9cbd7e037d42acc259abf61309dcb
Michael Korb <korb_michael@bah.com>
2025-03-07 08:48:55 -0500

    Adds client, etc.

--- services/passport.js
+++ services/passport.js
@@ -21,19 +21,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            // user exists
-            done(null, existingUser);
-          } else {
-            // create new user
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        })
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // user exists
+        return done(null, existingUser);
+      } 
+      // create new user
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
